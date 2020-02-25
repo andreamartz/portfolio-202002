4a64d9a675a38e43a762c0385d6e56eaaab544f0
Andrea Martz <14165788+wahmof2@users.noreply.github.com>
2020-02-25 10:07:07 -0600

    Add slide effect to nav

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,5 @@
 const navToggle = document.querySelector('.menu-button');
 
 navToggle.addEventListener('click', () => {
-  document.body.classList.add('nav-is-open');
+  document.body.classList.toggle('nav-is-open');
 })
